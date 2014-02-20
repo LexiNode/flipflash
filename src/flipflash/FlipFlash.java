e127648a0fa35fca388dbfee8a74bdf01a4360a9
spazfu <james.s.hooker@gmail.com>
2014-02-20 02:50:39 -0800

    Frame Icons
    Icons are not displayed in OSX as far as I can tell.  Testing is needed
    on other environments also.
    
    * Had a first merge conflict in the import section that I hopefully was
    able to resolve.

--- src/flipflash/FlipFlash.java
+++ src/flipflash/FlipFlash.java
@@ -5,12 +5,20 @@ package flipflash;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
@@ -22,6 +30,7 @@ import javax.swing.border.Border;
 public class FlipFlash {
 
 	JFrame frame = new JFrame("Flip Flash");
+	List<Image> icons = new ArrayList<Image>();
 	JPanel framePanel = new JPanel();
 	JPanel frontPanel = new JPanel();
 	JLabel frontLabel = new JLabel("Welcome to Flip Flash \npress button to begin");
@@ -40,6 +49,7 @@ public class FlipFlash {
 	/**
 	 * 
 	 */
+	
 	public FlipFlash() {
 		ffHelper = new FlipFlashHelper(this);
 		ffListener = new FlipFlashListener(this, ffHelper);
@@ -64,15 +74,51 @@ public class FlipFlash {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 		
+		addIcons(frame);
 		addAndArrangePanels(frame);
 		addMenu(frame);
 		
 		frame.pack();
 		frame.setVisible(true);
 	}
+	
+	/**
+	 * Sets the Icons for the Swing frame.
+	 * Icons are not displayed in OSX as far as I can tell.
+	 * Testing is needed on other environments also.
+	 * @param frame
+	 */
+	private void addIcons(JFrame frame) {
+		int[] macIconSizes = {16,32,64,128,256,512,1024};
+		for(int i : macIconSizes){
+			System.out.println(i);
+			icons.add(new ImageIcon("/flipflash/res/logo"+i+".png").getImage());
+		}
+		frame.setIconImages(icons);
+	}
 
 	private void addMenu(JFrame frame) {
-		// TODO Auto-generated method stub
+		JMenu file = new JMenu("File");
+		file.setMnemonic('F');
+		JMenuItem loadItem = new JMenuItem("Load Deck");
+		loadItem.setMnemonic('L');
+		loadItem.addActionListener(ffListener);
+		file.add(loadItem);
+		JMenuItem saveItem = new JMenuItem("Save Deck");
+		saveItem.setMnemonic('S');
+		saveItem.addActionListener(ffListener);
+		file.add(saveItem);
+		JMenuItem resetItem = new JMenuItem("Reset Deck");
+		resetItem.setMnemonic('R');
+		resetItem.addActionListener(ffListener);
+		file.add(resetItem);
+		JMenuItem exitItem = new JMenuItem("Exit");
+		exitItem.setMnemonic('x');
+		exitItem.addActionListener(ffListener);
+		file.add(exitItem);
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.add(file);
+		frame.setJMenuBar(menuBar);
 		
 	}
 
--- src/flipflash/FlipFlashHelper.java
+++ src/flipflash/FlipFlashHelper.java
@@ -19,12 +19,15 @@ public class FlipFlashHelper {
 
 	public void loadCards() {
 		// TODO Auto-generated method stub
-		ff.cards = new Flash[5];
+		ff.cards = new Flash[8];
 		ff.cards[0]=new Flash("a","あ");
 		ff.cards[1]=new Flash("i","い");
 		ff.cards[2]=new Flash("u","う");
 		ff.cards[3]=new Flash("e","え");
 		ff.cards[4]=new Flash("o","お");
+		ff.cards[5]=new Flash("The sentence on the other side of this card is true.","The sentence on the other side of this card is false.");
+		ff.cards[6]=new Flash("","");
+		ff.cards[7]=new Flash("","");
 		loadCard(0);
 	}
 	
